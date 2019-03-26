f9ad93940552bac29861f1d83d2e6603fe6fcf8f
王顺利 <wangshunli507@163.com>
2019-03-26 20:14:33 +0800

    ok

--- src/components/massagePage/interactive.vue
+++ src/components/massagePage/interactive.vue
@@ -1,92 +1,37 @@
 <template>
 <div>
-    
-        <!-- <ul>
-
-            <li class="dialog">
+       <ul>
+            <li class="dialog" v-for="(item,index) in page" :key="index" >
                
                 <div class="HeadPortrait">
-                     <img src="../../assets/img/001.jpg" alt="">
+                     <img :src="item.touxiang" alt="">
                 </div>
                
                 <div class="text-right">
-                    <span class="text">小茉莉正品韩妆</span>
-                    <span class="time">下午02:20</span>
-                    
+                    <span class="text">{{item.nicheng}}</span>
+                    <span class="time">{{item.time}}</span>
+                    <br><br><br>
+                    <span class="text2">{{item.text}}</span>  
                 </div>
             </li>
-             <li class="dialog">
-                <div class="HeadPortrait">
-                     <img src="../../assets/img/002.jpg" alt="">
-                </div>
-                <div class="text-right">
-                       <span class="text">Catherinezhu</span>
-                    <span class="time">下午01:46</span>
-                </div>
-            </li>
-             <li class="dialog">
-                <div class="HeadPortrait">
-                     <img src="../../assets/img/003.jpg" alt="">
-                </div>
-                <div class="text-right">
-                     <span class="text">ColorBlocker</span>
-                     <span class="time">下午01:38</span>
-                </div>
-            </li>
-             <li class="dialog">
-                <div class="HeadPortrait">
-                     <img src="../../assets/img/004.jpg" alt="">
-                </div>
-                <div class="text-right">
-                    <span class="text">香芋小姐</span>
-                    <span class="time">上午10:46</span>
-                </div>
-            </li>
-             <li class="dialog">
-                <div class="HeadPortrait">
-                     <img src="../../assets/img/005.jpg" alt="">
-                </div>
-                <div class="text-right">
-                    <span class="text">K.</span>
-                    <span class="time">昨天</span>
-                </div>
-            </li>
-             <li class="dialog">
-                <div class="HeadPortrait">
-                     <img src="../../assets/img/006.jpg" alt="">
-                </div>
-                <div class="text-right">
-                    <span class="text">澳洲代你</span>
-                    <span class="time">昨天</span>
-                </div>
-            </li>
-             <li class="dialog">
-                <div class="HeadPortrait">
-                     <img src="../../assets/img/007.jpg" alt="">
-                </div>
-                <div class="text-right">
-                    <span class="text">小叮韩屋</span>
-                    <span class="time">17/07/03</span>
-                </div>
-            </li>
-             <li class="dialog">
-                <div class="HeadPortrait">
-                     <img src="../../assets/img/008.jpg" alt="">
-                </div>
-                <div class="text-right">
-                    <span class="text">脸很大的兔子</span>
-                    <span class="time">17/07/03</span>
-                </div>
-            </li>
-        </ul> -->
-        
-    
+        </ul> 
 </div>
 </template>
 <script>
 
 export default {
     name:'interactive',
+    data(){
+        return{
+           page:[]
+        }
+    },
+    mounted(){
+        this.$axios.get(this.HOST+"/message")
+        .then(res =>{
+           this.page = res.data.dialoguePage;
+        })
+    }
 }
 </script>
 <style>
